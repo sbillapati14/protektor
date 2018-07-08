66fbc32ccf900021766244f8a908fd82e186f886
Robert Sliwa <robjsliwa@gmail.com>
2018-07-07 21:17:48 -0400

    Added test coverage

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,14 @@ module.exports = {
   coverageDirectory: './coverage',
   coverageReporters: ['lcov', 'text'],
   collectCoverageFrom: ['<rootDir>/src/**/*.{js,jsx}'],
+  coverageThreshold: {
+    global: {
+      statements: 85,
+      branches: 62,
+      functions: 90,
+      lines: 84
+    }
+  },
   testMatch: ['<rootDir>/spec/*.spec.js'],
   transform: {
     '^.+\\.jsx?$': 'babel-jest'
