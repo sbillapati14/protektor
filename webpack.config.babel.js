629ba499d4e90b86cd795d4e36920332691839de
Rob Sliwa <robjsliwa@gmail.com>
2018-07-11 11:34:10 -0400

    Updated library name

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -7,7 +7,7 @@ export default {
   output: {
     path: join(__dirname, 'dist'),
     libraryTarget: 'umd',
-    library: 'protektor'
+    library: '@iris-platform/protektor'
   },
   devtool: 'source-map',
   module: {
